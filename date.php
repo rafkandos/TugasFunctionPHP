0f7faa5dd6166739828addf9707657ff0af51f80
raffli <raffli@algostudio.net>
2021-03-23 19:57:19 +0700

    #TASK 4

--- date.php
+++ date.php
@@ -9,4 +9,7 @@
 
     //#TASK 3
     echo date("l, d-M-Y", mktime(0, 0, 0, date("m"), date("d") + 1000, date("Y"))) ."<br><br>";
+
+    //#TASK 4
+    echo date("l, d-M-Y", mktime(0, 0, 0, date("m"), date("d") - 500, date("Y"))) ."<br><br>";
 ?>
